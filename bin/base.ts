4a43edb2af7301d36aea3114427c687f5e779b6a
Kai Groner <kai@gronr.com>
2021-02-13 23:02:20 -0500

    Add a user+sudoer.

--- bin/base.ts
+++ bin/base.ts
@@ -4,4 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { BaseStack } from '../lib/base-stack';
 
 const app = new cdk.App();
-new BaseStack(app, 'BaseStack');
+const gronr = new BaseStack(app, 'GronrStack');
+
+const adminUser = gronr.node.tryGetContext('gronr/admin');
+if (adminUser)
+  gronr.makeAdmin(adminUser);
--- lib/base-stack.ts
+++ lib/base-stack.ts
@@ -1,3 +1,4 @@
+import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
 
 export class BaseStack extends cdk.Stack {
@@ -5,5 +6,39 @@ export class BaseStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+  }
+
+  users: Record<string, iam.User> = {};
+  roles: Record<string, iam.Role> = {};
+
+  addAdmin(name: string) {
+    const user = new iam.User(
+      this, 'user', {
+        userName: name,
+      });
+    user.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('job-function/ViewOnlyAccess'));
+    this.users[user.userName] = user;
+    this.makeAdmin(user);
+  }
+
+  makeAdmin(name: string | iam.User) {
+    const user = typeof name === 'string'
+      ? iam.User.fromUserName(this, 'user', name)
+      : name;
+
+    const role = new iam.Role(
+      this, 'sudoer', {
+        roleName: `sudo-${name}`,
+        assumedBy: new iam.ArnPrincipal(user.userArn)
+          .withConditions({
+            "Bool": {
+              "aws:MultiFactorAuthPresent": "true"
+            }
+          }),
+      });
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
+    this.roles[role.roleName] = role;
   }
 }
