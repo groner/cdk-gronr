899a1cc27b686951f979442eba265a027f5cb8e3
Kai Groner <kai@gronr.com>
2021-02-15 12:08:54 -0500

    Set account and region.

--- bin/gronr.ts
+++ bin/gronr.ts
@@ -4,7 +4,12 @@ import * as cdk from '@aws-cdk/core';
 import { GronrStack } from '../lib/gronr-stack';
 
 const app = new cdk.App();
-const gronr = new GronrStack(app, 'GronrStack');
+const gronr = new GronrStack(app, 'GronrStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
 
 const adminUser = gronr.node.tryGetContext('gronr/admin');
 if (adminUser)
